1eb0793dc0f9187442d3cdabf5201115f70a53e8
Matteo Semplice <matteo.semplice@uninsubria.it>
2021-07-08 17:35:43 +0200

    Destroy Vecs before their DM

--- src/appctx.cpp
+++ src/appctx.cpp
@@ -4,6 +4,14 @@
 
 PetscErrorCode cleanUpContext(AppContext & ctx){
   PetscErrorCode ierr;
+  //Destory Vecs before their DM!
+  ierr= VecDestroy(&ctx.U ); CHKERRQ(ierr);
+  ierr= VecDestroy(&ctx.U0); CHKERRQ(ierr);
+  ierr= VecDestroy(&ctx.F ); CHKERRQ(ierr);
+  ierr= VecDestroy(&ctx.F0); CHKERRQ(ierr);
+  ierr= VecDestroy(&ctx.Uloc); CHKERRQ(ierr);
+  ierr= VecDestroy(&ctx.PHIloc); CHKERRQ(ierr);
+
   ierr = DMDestroy(&ctx.daAll); CHKERRQ(ierr);
   //DMDA for fields
   ierr = DMDestroy(&ctx.daField[0]); CHKERRQ(ierr);
@@ -16,12 +24,6 @@ PetscErrorCode cleanUpContext(AppContext & ctx){
 
   ierr = MatDestroy(&ctx.J); CHKERRQ(ierr);
 
-  ierr= VecDestroy(&ctx.U ); CHKERRQ(ierr);
-  ierr= VecDestroy(&ctx.U0); CHKERRQ(ierr);
-  ierr= VecDestroy(&ctx.F ); CHKERRQ(ierr);
-  ierr= VecDestroy(&ctx.F0); CHKERRQ(ierr);
-  ierr= VecDestroy(&ctx.Uloc); CHKERRQ(ierr);
-  ierr= VecDestroy(&ctx.PHIloc); CHKERRQ(ierr);
 
   return ierr;
 }
